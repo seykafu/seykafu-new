79b064c419f526a03f10bff8115efdf49b5b591c
unknown <kaseykungfu@gmail.com>
2023-11-13 21:12:32 -0800

    wip

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import './assets/vendors/bootstrap/css/bootstrap.min.css';
+import './assets/vendors/font-awesome/css/font-awesome.min.css';
+import './assets/vendors/slick/slick.min.css';
+import './assets/vendors/slick/slick-theme.min.css';
+import './assets/vendors/animate.min.css';
+import './assets/vendors/magnific-popup/magnific-popup.css';
+import './assets/vendors/bootstrap-datepicker/css/bootstrap-datepicker.min.css';
+import './assets/vendors/fullscreen-overlay/css/style5.css';
+import './assets/vendors/lightbox/css/lightbox.min.css';
+import './assets/fonts/font-icon/style.css';
+import './assets/css/layout.css';
 import './assets/css/elements.css';
+import './assets/css/shortcode.css';
 import './assets/css/widget.css';
 import './assets/css/extra.css';
 import './assets/css/responsive.css';
-import './assets/vendors/bootstrap/css/bootstrap.min.css';
-import './assets/vendors/font-awesome/css/font-awesome.min.css';
-import './assets/fonts/font-icon/style.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import MainContent from './components/Main_Content';
+import BottomContent from './components/Bottom_Content';
+// import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
@@ -18,30 +30,10 @@ function App() {
       <header className="App-header">
         <body class="sticky-enable dark-mode">
           <div id="pagewrap" class="pagewrap">
-            <div class="col-lg-8">
+            
               <Header />
-                <div class="about-heading"><span class="heading-name">Kasey/JX Fu's Website!</span>
-                  <span class="heading-job"><span>/</span><span>PM, Writer, and Game + Manga fanatic!</span></span></div>
-                  <p>I'm a product manager, writer, aspiring entrepreneur, and game enthusiast (I like strategy and tactical RPG). I hold many interests and have worked on many side projects.</p>
-
-                  <h4>What do I do?</h4>
-                  <p>I'm a PM in the AI space. I was previously at Microsoft Bing, shipping AI models and features in their backend platform, and now I'm at Planview,
-                    shipping a new AI copilot for its broad client base. In general, I'm interested in industries such as gaming, enterprise, and content creation.
-                  </p>
-
-                  <h4>My other projects</h4>
-                <p>I'm a part-time writer & a published fiction author; my books include a YA Fantasy/Action titled <a href="https://www.amazon.com/dp/B0BJZXKMM9" 
-                    class="title"><u>"Darkness Me, Colorful You"</u></a> and a RomCom light novel titled 
-                    <a href="https://www.amazon.com/Corruptions-Are-Best-Exposed-Autumn-ebook/dp/B0BJ27R74M/ref=sr_1_1?crid=24XNNFKGALXA9&keywords=corruptions+are+best+exposed+in+the+autumn&qid=1665868843&qu=eyJxc2MiOiIwLjA2IiwicXNhIjoiMC4wMCIsInFzcCI6IjAuMDAifQ%3D%3D&sprefix=corruptions+are+best+exposed+in+the+autumn%2Caps%2C133&sr=8-1"> <u>"Corruptions Are Best Exposed In The Autumn!"</u>
-                    </a>
-                </p>
-                
-                  <h4>My Background</h4>
-                    <p>Before diving into writing and PM work, I graduated with a Bachelor's of Science in Science and Business from the University of Waterloo.
-                      I've completed many internships in product before graduating. I've been involved in many activities - you can see them all here. 
-                    </p>
-                  Here, you'll find more about me, my <a href= "https://seykafu.medium.com"> <u>Medium blog</u></a>, <a href="books.html"> <u>my books</u></a>, and my <a href="manga_reviews.html"> <u>manga review blog</u></a>!
-                </div>
+              <MainContent />
+              <BottomContent />
           </div>
         </body>
         <Footer />
---
+++ src/components/Bottom_Content.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import '../App.css';
+import '../assets/vendors/bootstrap/css/bootstrap.min.css';
+import '../assets/vendors/font-awesome/css/font-awesome.min.css';
+import '../assets/vendors/slick/slick.min.css';
+import '../assets/vendors/slick/slick-theme.min.css';
+import '../assets/vendors/animate.min.css';
+import '../assets/vendors/magnific-popup/magnific-popup.css';
+import '../assets/vendors/bootstrap-datepicker/css/bootstrap-datepicker.min.css';
+import '../assets/vendors/fullscreen-overlay/css/style5.css';
+import '../assets/vendors/lightbox/css/lightbox.min.css';
+import '../assets/fonts/font-icon/style.css';
+import '../assets/css/layout.css';
+import '../assets/css/elements.css';
+import '../assets/css/shortcode.css';
+import '../assets/css/widget.css';
+import '../assets/css/extra.css';
+import '../assets/css/responsive.css';
+import dmcy from '../assets/images/My Books/dmcyu_.jpeg';
+import medium from '../assets/images/medium.jpeg';
+import msft from '../assets/images/msft.jpeg';
+
+function BottomContent() {
+  return (
+          <div class="section-80">
+            <div class="container">
+              <div class="alay-sc-blog-list-cat style-cover">
+                <div class="row">
+                  <div class="col-lg-4 col-12">
+                    <div class="cat-item"><a href="books.html" class="img-wrap"><img src={dmcy} alt="Books"/>
+                        <h2 class="heading">My Books</h2></a></div>
+                  </div>
+                  <div class="col-lg-4 col-12">
+                    <div class="cat-item"><a href="writing.html" class="img-wrap"><img src={medium} alt="Writing"/>
+                        <h2 class="heading">My Articles</h2></a></div>
+                  </div>
+                  <div class="col-lg-4 col-12">
+                    <div class="cat-item"><a href="https://seykafu.notion.site/Seykafu-a8fda5021a274d82af74341b9a4a9f2e" class="img-wrap"><img src={msft} alt="Product"/>
+                        <h2 class="heading">Product Work</h2></a></div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+  );
+}
+
+export default BottomContent
